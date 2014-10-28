7bc6e882f3ca1d763ab18e276a62c8a083e29379
Sigmund Cherem <sigmund@google.com>
2014-10-28 16:33:01 -0700

    Update prehook - use script path as reference, fix pubspec comment

--- tool/uncomment_test_section_in_pubspec.dart
+++ tool/uncomment_test_section_in_pubspec.dart
@@ -2,15 +2,30 @@ library polymer.tool.uncomment_test_section_in_pubspec;
 
 import 'dart:io';
 
-main(args) {
-  var pubspec = new File('pubspec.yaml');
+import 'package:path/path.dart';
+
+main() {
+  var path = join(dirname(dirname(Platform.script.path)), 'pubspec.yaml');
+  print('uncomment_test_section_in_pubspec: processing $path');
+  var pubspec = new File(path);
   if (!pubspec.existsSync()) {
-    print("uncomment_test_section_in_pubspec error: pubspec doesn't exist.");
+    print('uncomment_test_section_in_pubspec error: '
+        'pubspec.yaml not found: $path.');
     exit(1);
   }
   var contents = pubspec.readAsStringSync();
   var begin = contents.indexOf("# ---prehook: begin uncomment---");
+  if (begin == -1) {
+    print('uncomment_test_section_in_pubspec error: '
+        'start of commented section not found.');
+    exit(1);
+  }
   var end = contents.indexOf("# ---prehook: end uncomment---", begin);
+  if (end == -1) {
+    print('uncomment_test_section_in_pubspec error: '
+        'end of commented section not found.');
+    exit(1);
+  }
   var newContents = new StringBuffer()
       ..write(contents.substring(0, begin))
       ..write(contents.substring(begin, end).replaceAll('\n#', '\n'))
