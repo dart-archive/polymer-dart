b0e95a2cf317c803016ad28bba3906e573d9661d
Don Olmstead <don.j.olmstead@gmail.com>
2015-07-13 11:46:34 -0700

    Fixing the signature of deserialize and adding checks to serialize

--- lib/src/common/polymer_serialize.dart
+++ lib/src/common/polymer_serialize.dart
@@ -11,19 +11,21 @@ import 'polymer_descriptor.dart';
 /// Mixin for Polymer serialization methods.
 ///
 /// This should only be used if the [serialize] and [deserialize] methods need
-/// to be overriden to support additional Dart types. Any types not explicitly
-/// handled by the overriden method should defer to the original method by
+/// to be overridden to support additional Dart types. Any types not explicitly
+/// handled by the overridden method should defer to the original method by
 /// calling the base class's implementation.
 abstract class PolymerSerialize implements PolymerMixin {
   JsObject get jsElement;
 
   /// Serializes the [value] into a [String].
   String serialize(Object value) {
-    return jsElement.callMethod('originalSerialize', [jsValue(value)]).toString();
+    var result = jsElement.callMethod('originalSerialize', [jsValue(value)]);
+
+    return (result != null) ? result.toString() : null;
   }
 
   /// Deserializes the [value] into an object of the given [type].
-  Object deserialize(String value, Type type) {
+  dynamic deserialize(String value, Type type) {
     return dartValue(jsElement.callMethod('originalDeserialize',
         [jsValue(value), jsType(type)]));
   }
