d0c7e7c8ad2b1abfa9fad10de8782d9b9ce86e8b
Jacob MacDonald <jakemac@google.com>
2015-07-28 10:19:57 -0700

    js_proxy updated to work with reflectable

--- test/src/common/js_proxy_test.dart
+++ test/src/common/js_proxy_test.dart
@@ -11,6 +11,7 @@ import 'package:test/test.dart';
 
 class EmptyModel {}
 
+@jsProxyReflectable
 class MyModel extends Object with JsProxy {
   int value = 0;
   int get readOnlyVal => 1;
@@ -19,6 +20,7 @@ class MyModel extends Object with JsProxy {
   int incrementBy([int amount = 1]) => value += amount;
 }
 
+@jsProxyReflectable
 class CachedMyModel extends MyModel {
   CachedMyModel() {
     useCache = true;
