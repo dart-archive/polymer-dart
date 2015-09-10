f217be1d8d98cc38969d1268c6fec07972e0617e
Jacob MacDonald <jakemac@google.com>
2015-09-10 12:18:47 -0700

    checkpoint, not fully working  yet

--- lib/init.dart
+++ lib/init.dart
@@ -5,6 +5,7 @@ library polymer.lib.init;
 
 import 'dart:async';
 import 'dart:js';
+import 'package:reflectable/reflectable.dart';
 import 'package:web_components/web_components.dart';
 import 'src/common/js_proxy.dart';
 import 'src/common/polymer_register.dart';
@@ -15,25 +16,39 @@ Future initPolymer() async {
   await initWebComponents(typeFilter: [HtmlImport], initAll: false);
   // Make sure `src/js/polymer_array_methods.html` is loaded first.
   _setUpListMethods();
+  _setUpPropertyChanged();
   await initWebComponents(
       typeFilter: [CustomElement, CustomElementProxy, PolymerRegister],
       initAll: true);
 }
 
+final _polymerDart = context['Polymer']['Dart'];
+
 void _setUpListMethods() {
-  var polymerDart = context['Polymer']['Dart'];
-  polymerDart['push'] = (List list, Iterable items) {
+  _polymerDart['push'] = (List list, Iterable items) {
     list.addAll(items.map((item) => dartValue(item)));
   };
-  polymerDart['pop'] = (List list) => list.removeLast();
-  polymerDart['shift'] = (List list) => list.removeAt(0);
-  polymerDart['unshift'] = (List list, Iterable items) {
+  _polymerDart['pop'] = (List list) => list.removeLast();
+  _polymerDart['shift'] = (List list) => list.removeAt(0);
+  _polymerDart['unshift'] = (List list, Iterable items) {
     list.insertAll(0, items.map((item) => dartValue(item)));
   };
-  polymerDart['splice'] =
+  _polymerDart['splice'] =
       (List list, int start, int deleteCount, Iterable items) {
     if (start < 0) start = list.length + start;
     if (deleteCount > 0) list.removeRange(start, start + deleteCount);
     list.insertAll(start, items.map((item) => dartValue(item)));
+  };
+}
+
+void _setUpPropertyChanged() {
+  _polymerDart['propertyChanged'] = (dartInstance, String path, newValue) {
+    var instanceMirror = jsProxyReflectable.reflect(dartValue(dartInstance));
+    // Read only property?
+    if (instanceMirror.type.instanceMembers['$path'] is! VariableMirror &&
+        instanceMirror.type.instanceMembers['$path='] == null) {
+      return;
+    }
+    instanceMirror.invokeSetter(path, dartValue(newValue));
   };
 }
